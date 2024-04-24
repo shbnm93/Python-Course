4b0de7de3b43592dd4c916fb4729df23eaef5ba2
shbnm93 <shabnamhamidi1993@gmail.com>
2024-04-24 16:42:45 +0300

    file

---
+++ file-io.py
@@ -0,0 +1,12 @@
+# data = open("d:/mydata.txt", "r")
+# data = open("d:/mydata.txt", "r+")
+# data = open("d:/mydata.txt", "w")
+# data = open("d:/mydata.txt", "w+")
+# data = open("d:/mydata.txt", "x")
+# data = open("d:/mydata.txt", "rt")
+# data = open("d:/mydata.txt", "rb")
+# data = open("d:/mydata.txt", "r+b")
+
+
+data = open("mydata.txt", "r+")
+print(data.read())
